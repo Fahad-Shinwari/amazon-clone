6ed71d21e2416d9cf15ce37ada1f56d64973cef3
Fahad-Shinwari <shinwarifahad96@gmail.com>
2021-08-04 15:04:30 +0430

    Added Products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-function App() {
-  return (
-    <div className="App">
-        <div className="header">Header</div>
-        <div className="main">Body</div>
-        <div className="footer">Footer try</div>
-    </div>
-  );
+import React from "react";
+import Products from "./components/Products";
+import data from './data.json'
+
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      products:data.products,
+      size:"",
+      sort:""
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+          <header>Header</header>
+          <main>
+            <div className="content">
+                <div className="main"><Products products={this.state.products} /></div>
+                <div className="sidebar">Cart Items</div>
+            </div>
+          </main>
+          <footer>Footer try</footer>
+      </div>
+    );
+  }   
 }
 
 export default App;
